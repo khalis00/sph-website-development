997654388cb8e28697f39cdbf209c886322d7ed2
FaithKaruga12 <karugafaith6@gmail.com>
2024-06-12 01:05:48 -1200

    further editing

--- src/components/landing/FeatureActions.js
+++ src/components/landing/FeatureActions.js
@@ -20,9 +20,18 @@ const FeatureActions = () => {
         .img-fluid:hover {
           transform: scale(1.05);
         }
+        .container-white {
+          background-color: white;
+        }
+        .bg-white {
+          background-color: white;
+        }
+        .text-dark {
+          color: #000;
+        }
       `}</style>
       {/* Features */}
-      <div className="container overflow-hidden content-space-1">
+      <div className="container container-white overflow-hidden content-space-1">
         {/* Heading */}
         <div className="w-lg-65 text-center mx-lg-auto mb-7">
           <h3>
@@ -38,18 +47,15 @@ const FeatureActions = () => {
         {/* End Heading */}
         
         <div className="content-space-b-1 content-space-b-md-1">
-          <div className="bg-soft-primary-light">
+        <div className="bg-white position-relative pt-3 pt-md-0">
             <div className="container">
               <div className="row justify-content-center justify-content-lg-between">
                 <div className="col-md-6 content-space-t-1 content-space-md-1">
                   {/* Heading */}
-                  <div className="mb-7">
-                    <h1 className="display-4 text-primary mb-3 mb-md-5">
-                      <span className="text-warning">Enhancing  —</span>
-                      <br />
-                      Youth Civic Participation and Community Impact
-                    </h1>
-                    <div className="lead">
+                  <div className="pe-lg-3 mb-7">
+                      <h3 className="text-dark">Enhancing —
+                      Youth Civic Participation and Community Impact</h3>
+                      <div className="lead text-dark">
                       <ul>
                         <li>Providing safe and conducive spaces for youth to engage in civic activities and express their ideas.</li>
                         <li>Offering holistic support systems, including mentorship programs, to empower youth and enhance their skills for community involvement.</li>
@@ -68,25 +74,25 @@ const FeatureActions = () => {
                         <div
                           className="d-none d-md-block bg-img-center"
                           style={{
-                            backgroundImage: "url(/assets/images/1.jpg.jpeg)",
+                            backgroundImage: "url(/assets/images/beach1.jpg)",
                             height: "18.25rem"
                           }}
                         />
                         <div
                           className="d-none d-md-block bg-img-center"
                           style={{
-                            backgroundImage: "url(/assets/images/2.jpg.jpeg)",
+                            backgroundImage: "url(/assets/images/beach2.jpg)",
                             height: "20.25rem"
                           }}
                         />
                         <img
                           className="d-md-none img-fluid w-100"
-                          src="/assets/images/3.jpg.jpeg"
+                          src="/assets/images/beach3.jpg"
                           alt="Image Description"
                         />
                         <img
                           className="d-md-none img-fluid w-100"
-                          src="/assets/images/2.jpg.jpeg"
+                          src="/assets/image/beach4.jpg"
                           alt="Image Description"
                         />
                         {/* End Images */}
@@ -99,25 +105,25 @@ const FeatureActions = () => {
                         <div
                           className="d-none d-md-block bg-img-center"
                           style={{
-                            backgroundImage: "url(/assets/images/1.jpg.jpeg)",
+                            backgroundImage: "url(/assets/images/beach3.jpg)",
                             height: "22rem"
                           }}
                         />
                         <div
                           className="d-none d-md-block bg-img-center"
                           style={{
-                            backgroundImage: "url(/assets/images/2.jpg.jpeg)",
+                            backgroundImage: "url(/assets/images/beach4.jpg)",
                             height: "16rem"
                           }}
                         />
                         <img
                           className="d-md-none img-fluid w-100"
-                          src="/assets/images/1.jpg.jpeg"
+                          src="/assets/images/collabo1.jpg"
                           alt="Image Description"
                         />
                         <img
                           className="d-md-none img-fluid w-100"
-                          src="/assets/images/2.jpg.jpeg"
+                          src="/assets/images/collabo2.jpg"
                           alt="Image Description"
                         />
                         {/* End Images */}
@@ -132,99 +138,22 @@ const FeatureActions = () => {
               {/* End Row */}
             </div>
           </div>
-<<<<<<< HEAD
-=======
-          {/* End Col */}
-          <div className="col-sm-10 col-md-6 col-lg-5">
-            <div className="row gx-2 transform-y-5rem">
-              <div className="col-6">
-                <div className="d-grid gap-2">
-                  {/* Images */}
-                  <div
-                    className="d-none d-md-block bg-img-center"
-                    style={{
- aliisph
-                      backgroundImage:"url(../assets/img/225x324/img1.jpg)",
-                  
-
-                      backgroundImage: "url(../assets/img/225x292/img1.jpg)",
-                      height: "18.25rem"
- alpha-dev
-                    }}
-                  />
-                  <div
-                    className="d-none d-md-block bg-img-center"
-                    style={{
-                      backgroundImage: "url(../assets/img/225x324/img1.jpg)",
-                      height: "20.25rem"
-                    }}
-                  />
-                  <img
-                    className="d-md-none img-fluid w-100"
-                    src="../assets/img/225x292/img1.jpg"
-                    alt="Image Description"
-                  />
-                  <img
-                    className="d-md-none img-fluid w-100"
-                    src="../assets/img/225x324/img1.jpg"
-                    alt="Image Description"
-                  />
-                  {/* End Images */}
-                </div>
-              </div>
-              {/* End Col */}
-              <div className="col-6">
-                <div className="d-grid gap-2">
-                  {/* Images */}
-                  <div
-                    className="d-none d-md-block bg-img-center"
-                    style={{
-                      backgroundImage: "../assets/img/225x324/img1.jpg",
-                      height: "22rem"
-                    }}
-                  />
-                  <div
-                    className="d-none d-md-block bg-img-center"
-                    style={{
-                      backgroundImage: "url(../assets/img/225x261/img1.jpg)",
-                      height: "16rem"
-                    }}
-                  />
-                  <img
-                    className="d-md-none img-fluid w-100"
-                    src="../assets/img/225x355/img1.jpg"
-                    alt="Image Description"
-                  />
-                  <img
-                    className="d-md-none img-fluid w-100"
-                    src="../assets/img/225x261/img1.jpg"
-                    alt="Image Description"
-                  />
-                  {/* End Images */}
-                </div>
-              </div>
-              {/* End Col */}
-            </div>
-            {/* End Row */}
-          </div>
-          {/* End Col */}
->>>>>>> 6bab4bf35dc7a4915e9c95e022b3416b0e458fa7
         </div>
         {/* End Hero */}
       
         {/* Features */}
-        <div className="container overflow-hidden content-space-b-1">
+        <div className="container container-white overflow-hidden content-space-b-1">
           <div className="row justify-content-end align-items-md-center">
-            <div className="bg-primary-dark position-relative pt-3 pt-md-0">
+            <div className="bg-white position-relative pt-3 pt-md-0">
               <div className="container">
                 <div
                   className="d-none d-md-block col-md-6 position-absolute top-0 end-0 bg-img-center h-100"
-                  style={{ backgroundImage: "url(/assets/images/2.jpg.jpeg)" }}
+                  style={{ backgroundImage: "url(/assets/images/collabo1.jpg)" }}
                 />
                 <div className="d-md-none mb-5 mb-md-0">
                   <img
                     className="img-fluid"
-                    src="/assets/images/1.jpg.jpeg"
+                    src="/assets/images/collabo2.jpg"
                     alt="Image Description"
                   />
                 </div>
@@ -232,14 +161,13 @@ const FeatureActions = () => {
                   <div className="col-md-8 col-lg-6">
                     {/* Heading */}
                     <div className="pe-lg-3 mb-7">
-                      <h3>Strengthening Partnerships and Collaborations for Youth Empowerment</h3>
-                      <div className="lead text-white">
+                      <h3 className="text-dark">Strengthening Partnerships and Collaborations for Youth Empowerment</h3>
+                      <div className="lead text-dark">
                         <ul>
                           <li>Fostering partnerships between various stakeholders, such as government institutions, NGOs, and community organizations, to create a comprehensive support system for youth.</li>
                           <li>Collaborating with different sectors to increase opportunities for young people, such as education, employment, and entrepreneurship.</li>
                           <li>Catalyzing systemic change by bringing together diverse actors to collectively address issues affecting youth and create sustainable solutions.</li>
                         </ul>
-                        <span className="text-warning">destination?</span>
                       </div>
                     </div>
                     {/* End Heading */}
@@ -256,7 +184,7 @@ const FeatureActions = () => {
                     >
                       <img
                         className="img-fluid"
-                        src="/assets/images/2.jpg.jpeg"
+                        src="/assets/images/collabo2.jpg"
                         alt="Image Description"
                       />
                     </div>
@@ -270,29 +198,29 @@ const FeatureActions = () => {
           {/* End Row */}
         </div>
         {/* End Features */}
-      
+      <br></br><br></br>
         {/* Features */}
-        <div className="container overflow-hidden content-space-b-1">
-          <div className="bg-soft-warning position-relative py-4 py-md-0">
+        <div className="container container-white overflow-hidden content-space-b-1">
+        <div className="bg-white position-relative pt-3 pt-md-0">
             <div className="container">
               <div
                 className="d-none d-md-block col-md-6 position-absolute top-0 end-0 bg-img-center h-100"
-                style={{ backgroundImage: "url(/assets/images/2.jpg.jpeg)" }}
+                style={{ backgroundImage: "url(/assets/images/sports1.jpg)" }}
               />
               <div className="d-md-none mb-5 mb-md-0">
                 <img
                   className="img-fluid"
-                  src="/assets/images/1.jpg.jpeg"
+                  src="/assets/images/sports1.jpg"
                   alt="Image Description"
                 /><br></br>
               </div>
               <div className="row align-items-md-center content-space-md-3">
                 <div className="col-md-6">
                   <div className="pe-lg-3">
-                    <h1 className="h2 mb-3 mb-md-5">
+                    <h3 className="pe-mb-3 mb-md-7">
                       Promoting Positive Youth Development and Building Capacities
-                    </h1>
-                    <div>
+                    </h3>
+                    <div className="lead text-dark">
                       <ul>
                         <li>Implementing youth development programs that focus on nurturing the skills, capacities, and agency of young individuals.</li>
                         <li>Offering opportunities for young people to develop their talents, interests, and strengths through a range of activities and initiatives.</li>
@@ -313,7 +241,7 @@ const FeatureActions = () => {
                   >
                     <img
                       className="img-fluid"
-                      src="/assets/images/2.jpg.jpeg"
+                      src="/assets/images/sports2.jpg"
                       alt="Image Description"
                     />
                   </div>
--- src/components/landing/SwahilipotPillar.js
+++ src/components/landing/SwahilipotPillar.js
@@ -5,76 +5,71 @@ const SwahilipotPillar = () => {
     <>
       {/* Swahilipot Pillars */}
       <div className='bg-soft-success p-2'>
-        <div className='w-lg-65 text-center mt-4 mx-lg-auto mb-7'>
+        <div className='w-lg-65 text-center mt-4 mx-lg-auto mb-4'> {/* Reduced mb-7 to mb-4 */}
           <h3>Programs and Activities</h3>
         </div>
         <div className='position-relative'>
-          <div className='container content-space-1 content-space-md-3'>
+        <div className='container-fluid content-space-0 content-space-md-1 d-flex justify-content-center align-items-center'>
+
             <div className='row'>
               <div className='col-md-7'>
-                <div className='row'>
-                  <div className='col-sm-6 mb-3'>
-                    <div className='pe-md-3'>
-                      <h5>Technology and Innovation</h5>
-                      <p>
-                        Pwani Innovation Week: Showcasing coastal Kenya's
-                        technology and innovation. Innovation challenges and
-                        hackathons: Collaborative events solving problems with
-                        technology. Technology academy: Training in networking,
-                        software development, and digital literacy. Coding
-                        contests: Showcasing coding skills.
-                      </p>
-                    </div>
-                  </div>
-                  {/* End Col */}
-                  <div className='col-sm-6 mb-3'>
-                    <div className='pe-md-3'>
-                      <h5>Entrepreneurship</h5>
-                      <p>
-                        Pitching Thursday offers a platform for entrepreneurs to
-                        present their ideas and receive expert feedback. Info
-                        Sessions provide guidance and resources for aspiring
-                        business owners. Advisory services, technical
-                        assistance, and linkages offer support in finance and
-                        market connections. Startup School provides
-                        comprehensive training for successful startup ventures.
-                      </p>
-                    </div>
-                  </div>
-                  {/* End Col */}
+                <div className='pe-md-3 mb-3'>
+                  <h5>Technology and Innovation</h5>
+                  <p>
+                    Pwani Innovation Week: Showcasing coastal Kenya's
+                    technology and innovation. Innovation challenges and
+                    hackathons: Collaborative events solving problems with
+                    technology. Technology academy: Training in networking,
+                    software development, and digital literacy. Coding
+                    contests: Showcasing coding skills.
+                  </p>
                 </div>
-                {/* End Row */}
-                <div className='row'>
-                  <div className='col-sm-6 mb-3 mb-sm-0'>
-                    <div className='pe-md-3'>
-                      <h5>Creatives and Arts</h5>
-                      <p>
-                        Pwani Gat Talent offers local artists a platform to
-                        showcase their skills. CASIK provides dedicated spaces
-                        for creative expression. Friday Poetry is a regular
-                        event for poets and spoken word artists to perform. Koo
-                        Store serves as a creative hub and marketplace for
-                        artists and designers to sell their unique creations.
-                      </p>
-                    </div>
-                  </div>
-                  {/* End Col */}
-                  <div className='col-sm-6'>
-                    <div className='pe-md-3'>
-                      <h5>Heritage</h5>
-                      <p>
-                        MoHoA preserves and celebrates modern African cultural
-                        heritage. Heritopolis promotes preservation and
-                        documentation of heritage sites. Youth.Heritage.Africa
-                        engages youth in heritage conservation. Ode to the
-                        Ancestors is a cultural event honoring community
-                        heritage.
-                      </p>
-                    </div>
-                  </div>
-                  {/* End Col */}
+              </div>
+              {/* End Col */}
+              <div className='col-md-5'>
+                <div className='pe-md-3 mb-3'>
+                  <h5>Entrepreneurship</h5>
+                  <p>
+                    Pitching Thursday offers a platform for entrepreneurs to
+                    present their ideas and receive expert feedback. Info
+                    Sessions provide guidance and resources for aspiring
+                    business owners. Advisory services, technical
+                    assistance, and linkages offer support in finance and
+                    market connections. Startup School provides
+                    comprehensive training for successful startup ventures.
+                  </p>
                 </div>
-                {/* End Row */}
+              </div>
+              {/* End Col */}
+            </div>
+            {/* End Row */}
+            <div className='row'>
+              <div className='col-md-5'>
+                <div className='pe-md-3 mb-3'>
+                  <h5>Creatives and Arts</h5>
+                  <p>
+                    Pwani Gat Talent offers local artists a platform to
+                    showcase their skills. CASIK provides dedicated spaces
+                    for creative expression. Friday Poetry is a regular
+                    event for poets and spoken word artists to perform. Koo
+                    Store serves as a creative hub and marketplace for
+                    artists and designers to sell their unique creations.
+                  </p>
+                </div>
+              </div>
+              {/* End Col */}
+              <div className='col-md-7'>
+                <div className='pe-md-3'>
+                  <h5>Heritage</h5>
+                  <p>
+                    MoHoA preserves and celebrates modern African cultural
+                    heritage. Heritopolis promotes preservation and
+                    documentation of heritage sites. Youth.Heritage.Africa
+                    engages youth in heritage conservation. Ode to the
+                    Ancestors is a cultural event honoring community
+                    heritage.
+                  </p>
+                </div>
               </div>
               {/* End Col */}
             </div>
@@ -103,4 +98,4 @@ const SwahilipotPillar = () => {
   )
 }
 
-export default SwahilipotPillar
+export default SwahilipotPillar;

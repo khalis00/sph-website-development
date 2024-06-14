658f50376217c8220eef1f56ae85dd2db1941ff8
FaithKaruga12 <karugafaith6@gmail.com>
2024-06-14 04:09:56 -1200

    imported and animated countUp member nunbers.placed FAQS section in cards and animated them

--- src/components/landing/Numbers.js
+++ src/components/landing/Numbers.js
@@ -1,19 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import CountUp from 'react-countup'
+
 const Numbers = () => {
+  const [key, setKey] = useState(0)
+
+  // Auto-refresh the component every 2 minutes
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKey(prevKey => prevKey + 1)
+    }, 30000) // 30000 milliseconds = 0.5 minutes
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className='bg-soft-warning'>
       <div className='container content-space-1 content-space-md-3'>
         {/* Heading */}
-        <div className='w-lg-65 text-center mx-lg-auto mb-7'>
+        <div className='w-lg-65 text-center mx-lg-auto mb-4'>
           <h3>Swahilipot Hub in Numbers</h3>
           <p className='fs-6'>Our Impacts in Numbers</p>
         </div>
         {/* End Heading */}
         <div className='w-lg-75 text-center mx-lg-auto'>
-          <div className='row col-sm-divider'>
+          <div className='row'>
             <div className='col-sm-4'>
               {/* Info */}
               <div className='text-center p-3'>
-                <h2 className='text-primary'>4000+</h2>
+                <h2 className='text-primary'>
+                  <CountUp key={key} start={0} end={4000} duration={2} />
+                  +
+                </h2>
                 <p className='mb-0'>Community Members</p>
               </div>
               {/* End Info */}
@@ -22,7 +39,9 @@ const Numbers = () => {
             <div className='col-sm-4'>
               {/* Info */}
               <div className='text-center p-3'>
-                <h2 className='text-primary'>25</h2>
+                <h2 className='text-primary'>
+                  <CountUp key={key} start={0} end={25} duration={2} />
+                </h2>
                 <p className='mb-0'>Programs and Activities</p>
               </div>
               {/* End Info */}
@@ -31,7 +50,9 @@ const Numbers = () => {
             <div className='col-sm-4'>
               {/* Info */}
               <div className='text-center p-3'>
-                <h2 className='text-primary'>65%</h2>
+                <h2 className='text-primary'>
+                  <CountUp key={key} start={0} end={65} duration={2} />%
+                </h2>
                 <p className='mb-0'>of our members are Youth</p>
               </div>
               {/* End Info */}
--- src/components/shared/SubscriberForm.js
+++ src/components/shared/SubscriberForm.js
@@ -1,44 +1,53 @@
-import React, { useState } from 'react'
-import { useRouter } from 'next/router'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 const SubscriberForm = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-  })
+  });
 
   const [submitted, setSubmitted] = useState(false); // State to track form submission
+  const [submitting, setSubmitting] = useState(false); // State to track form submission progress
 
-  const handleChange = e => {
-    const { name, value } = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+  };
 
-  const handleSubmit = async e => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitting(true); // Start form submission progress
+
     try {
       const response = await axios.post(
-          `${process.env.NEXT_PUBLIC_BASE_URL}${process.env.NEXT_PUBLIC_SUBSCRIBERS_FORM}`,
+        `${process.env.NEXT_PUBLIC_BASE_URL}${process.env.NEXT_PUBLIC_SUBSCRIBERS_FORM}`,
         formData
-      )
+      );
+
       // Clear form data after successful submission
       setFormData({
         name: '',
         email: '',
-      })
+      });
+
       // Redirect to the success page
-      router.push('/success')
-      setSubmitted(true); // Set submitted state to true after successful submission
+      router.push('/success');
+
+      // Set submitted state to true after successful submission
+      setSubmitted(true);
     } catch (error) {
-      console.error('Error:', error)
+      console.error('Error:', error);
+    } finally {
+      setSubmitting(false); // Complete form submission progress
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -54,7 +63,6 @@ const SubscriberForm = () => {
           placeholder='Type your name'
           aria-label='Type your name'
           required
-          data-msg='Please enter your name.'
           value={formData.name}
           onChange={handleChange}
         />
@@ -72,15 +80,14 @@ const SubscriberForm = () => {
           placeholder='example@site.com'
           aria-label='example@site.com'
           required
-          data-msg='Please enter a valid email address.'
           value={formData.email}
           onChange={handleChange}
         />
       </div>
 
       <div className='d-grid text-center'>
-        <button type='submit' className='btn btn-primary btn-lg'>
-          SUBMIT
+        <button type='submit' className='btn btn-primary btn-lg' disabled={submitting}>
+          {submitting ? 'Submitting...' : 'SUBMIT'}
         </button>
       </div>
       {submitted && <p>Form submitted successfully!</p>} {/* Display message if form submitted */}
@@ -91,7 +98,7 @@ const SubscriberForm = () => {
         </a>
       </span>
     </form>
-  )
-}
+  );
+};
 
-export default SubscriberForm
+export default SubscriberForm;
